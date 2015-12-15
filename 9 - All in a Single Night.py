2d55e00fd439ee97722ff8d2f47ab7c85c31741e
Liana Lo <liana.lo@prezi.com>
2015-12-15 14:28:47 +0100

    Day 9 - clean up

--- 9 - All in a Single Night.py
+++ 9 - All in a Single Night.py
@@ -11,52 +11,8 @@ def add_to_graph(city1, city2, distance, graph):
     if graph.get(city1):
         graph[city1][city2] = int(distance)
     else:
-        graph[city1] = {city2: int(distance)}
+        graph[city1] = { city2: int(distance) }
     return graph
-
-
-# not relevant? 
-def find_minimum_distance(city1, city2, graph, current_distance=0):
-    next_cities = graph[city1].keys()
-    if city2 in next_cities:
-        return current_distance + graph[city1][city2]
-
-
-def get_one_path_distance(starting_city, visited, distance, graph):
-    print ''
-    print starting_city
-    visited.add(starting_city)
-    more_cities_to_visit = set(graph[starting_city].keys()) - visited
-    print more_cities_to_visit
-
-    if more_cities_to_visit == set([]):
-        print 'YO', starting_city
-        return 0
-
-    # for city in more_cities_to_visit:
-        # print distance
-    city = list(more_cities_to_visit)[0]
-    print graph[city][starting_city]
-    distance += graph[city][starting_city]
-    print 'ditance: ', distance
-    distance += get_one_path_distance(city, visited, distance, graph)
-    print 'ditance: ', distance
-
-
-    return distance
-
-
-
-# def get_min_distance_to_reach_all_cities(graph, visited):
-#     if len(visited) == len(graph.keys()):
-#         return 0
-
-#     path_distances = []
-#     all_cities = set(graph.keys())
-#     for city in all_cities - visited:
-#         min_for_this_city = get_min_distance_to_reach_all_cities(graph, visited.add(city))
-#         path_distances.append(min_for_this_city)
-#     return min(path_distances)
 
 
 def dfs(graph, starting_city, visited=None):
@@ -68,21 +24,22 @@ def dfs(graph, starting_city, visited=None):
     return visited
 
 
-graph = {'Belfast': {'Dublin': 141, 'London': 518},
-            'Dublin': {'Belfast': 141, 'London': 464},
-            'London': {'Belfast': 518, 'Dublin': 464}}
-
-
-print dfs(graph, 'Belfast')
-
-
+def my_permutations(l):
+    if len(l) < 2:
+        return [l]
+    permutations = []
+    for item in l:
+        remaining_items = l[:]
+        remaining_items.remove(item)
+        rest = my_permutations(remaining_items)
+        permuations_starting_with_this_item = [[item] + x for x in rest]
+        permutations.extend(permuations_starting_with_this_item)
+    return permutations
 
 
 # https://mchouza.wordpress.com/2010/11/21/solving-the-travelling-salesman-problem/
-
-
-
 from itertools import permutations
+
 
 def tour_len(path, graph):
     distance = 0
@@ -92,19 +49,19 @@ def tour_len(path, graph):
         distance += graph[city1][city2]
     return distance
 
+
 def tsp_naive_solve(graph):
     shortest_distance, best_tour = min((tour_len(t, graph), t) for t in permutations(graph.keys()))
     return shortest_distance, best_tour
 
-# print tsp_naive_solve(graph)
 
-
-
-
-
+def tsp_naive_solve_get_shortest_distance_only(graph):
+    shortest_distance = min((tour_len(t, graph)) for t in my_permutations(graph.keys()))
+    return shortest_distance
 
 
 import unittest
+
 
 class Test(unittest.TestCase):
     def setUp(self):
@@ -112,29 +69,34 @@ class Test(unittest.TestCase):
             London to Belfast = 518
             Dublin to Belfast = 141'''
 
-        self.graph = {'Belfast': {'Dublin': 141, 'London': 518},
+        self.graph = {
+            'Belfast': {'Dublin': 141, 'London': 518},
             'Dublin': {'Belfast': 141, 'London': 464},
-            'London': {'Belfast': 518, 'Dublin': 464}}
+            'London': {'Belfast': 518, 'Dublin': 464}
+        }
 
     def test_make_graph(self):
         self.assertEqual(make_graph(self.s), self.graph)
 
-    def test_find_minimum_distance(self):
-        self.assertEqual(get_min_distance_to_reach_all_cities(self.graph, set([])), 141)
+    def test_dsf(self):
+        self.assertEqual(dfs(self.graph, 'Dublin'), set(['Dublin', 'London', 'Belfast']))
 
-    def test_get_one_path_distance(self):
-        self.assertEqual(get_one_path_distance('Belfast', set([]), 0, self.graph), 35)
+    def test_tsp_naive_solve(self):
+        self.assertEqual(tsp_naive_solve(self.graph), (605, ('Belfast', 'Dublin', 'London')))
 
-# unittest.main()
-s = open('input.txt', 'r').read()
+    def test_my_permutations(self):
+        self.assertEqual(my_permutations(self.graph.keys()), [list(l) for l in permutations(self.graph.keys())])
 
-# s = '''London to Dublin = 464
-# London to Belfast = 518
-# Dublin to Belfast = 141'''
+    def test_tsp_naive_solve_get_shortest_distance_only(self):
+        self.assertEqual(tsp_naive_solve_get_shortest_distance_only(self.graph), 605)
 
-graph = make_graph(s)
+
+unittest.main()
+
+# s = open('input.txt', 'r').read()
+# graph = make_graph(s)
 
 # from pprint import pprint
 # pprint(graph)
 
-print tsp_naive_solve(graph)
+# print tsp_naive_solve(graph)
